dfdc25d310a289d67b1636a71a888a2d9b4c9c6f
Sébastien Doeraene <sjrdoeraene@gmail.com>
2012-03-16 14:10:50 +0100

    Initial commit.

---
+++ main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <mozartcore.hh>
+#include <coreinterfaces.hh>
+
+using namespace std;
+using namespace mozart;
+
+int main(int argc, char **argv) {
+  // Create the virtual machine
+  VirtualMachine virtualMachine(nullptr);
+  VM vm = &virtualMachine;
+
+  // Experiment
+  UnstableNode unstable;
+  unstable.make<SmallInt>(vm, 5);
+  StableNode stable;
+  stable.init(vm, unstable);
+  cout << stable.type()->getName() << endl;
+}
